ec2864f1235cc2162fd6dcc9e12456b86e101d54
frankda <suyunyi@gmail.com>
2022-05-22 14:07:26 +1000

    v1 update

---
+++ pages/index.styles.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const CalculatorContainer = styled.div`
+  border: 1px solid white;
+  border-radius: 10px;
+  padding: 16px;
+`
+
+export const Selector = styled.select`
+  margin-left: 4px;
+`
+
+export const UnitContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 2px;
+`
+
+export const Unit = styled.div`
+  padding-right: 10px;
+`
+
+export const LivePriceContainer = styled.div`
+  display: flex;
+  margin-bottom: 8px;
+`
+
+export const LivePrice = styled.span`
+  margin-right: 8px;
+`
